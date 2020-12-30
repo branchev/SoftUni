32120a147e793b59e8b250d322251fa21905358a
branchev <76597625+branchev@users.noreply.github.com>
2020-12-30 14:32:17 +0200

    Update dsktop_app.py

--- python_fundamentals/dsktop_app.py
+++ python_fundamentals/dsktop_app.py
@@ -5,6 +5,9 @@ from tkinter.ttk import *
 from tkinter import *
 from tkcalendar import DateEntry
 from tkinter.scrolledtext import *
+import json
+
+all_tasks_to_write = []
 
 
 def clear_view(tk):
@@ -12,6 +15,10 @@ def clear_view(tk):
     # algorithm below creates the new interface of the window for the task form
     for s in tk.grid_slaves():
         s.destroy()
+
+
+def edit_task(tk):
+    clear_view(tk)
 
 
 # the parameter *args unpacks the given arguments as tuple,
@@ -22,9 +29,10 @@ def clear_view(tk):
 
 # kwargs unpacks the arguments as named key-value pairs in dictionary
 def new_task(**kwargs):
-    print(kwargs)
+    all_tasks_to_write.append(kwargs)
 
 
+# this is a render of the page when you click create task button
 def create_task(tk):
     clear_view(tk)
     Label(tk, text="Enter your task name:").grid(row=0, column=0, padx=20, pady=20)
@@ -52,6 +60,8 @@ def create_task(tk):
     Checkbutton(tk, text="Check", variable=is_completed).grid(row=6, column=1)
     # the description must be a multiline string that's why below it is defined that it will contains all the characters
     # from the first char to the end - (1.0 - END)
+    # lambda is used for referring to a function with given arguments - to call the function by "on click action"
+    # if there is not a lambda, we can refer without brackets (), but the function needs arguments to be useful
     Button(
         tk, text="Create task", bg="yellow", fg="red",
         command=lambda: new_task(
@@ -65,9 +75,17 @@ def create_task(tk):
 
 
 def main_view(tk):
-    Button(tk, text="List Tasks", bg="yellow", fg="red").grid(row=0, column=0, padx=20, pady=20)
-    Button(tk, text="New Task", bg="yellow", fg="red", command=lambda: create_task(tk)).grid(row=0, column=1, padx=20,
-                                                                                             pady=20)
+    Button(
+        tk,
+        text="List Tasks", bg="yellow", fg="red",
+        command=lambda: edit_task(tk)
+    ).grid(row=0, column=0, padx=20, pady=20)
+
+    Button(
+        tk,
+        text="New Task", bg="yellow", fg="red",
+        command=lambda: create_task(tk)
+    ).grid(row=0, column=1, padx=20, pady=20)
 
 
 tk = Tk()
@@ -75,3 +93,17 @@ tk.geometry("400x700")
 
 main_view(tk)
 tk.mainloop()
+
+# r+ is read and write mode:
+with open("db.txt", "r+") as file:
+    try:
+        existing_tasks = json.load(file)
+    except json.decoder.JSONDecodeError:
+        existing_tasks = []
+    # the seek function means to put the cursor at the chosen point
+    file.seek(0)
+    # truncate cleans all the contain from the file
+    file.truncate()
+    all_tasks_to_write.extend(existing_tasks)
+    # dump method puts lists, dicts etc. to the database file without parsing it to string.
+    json.dump(all_tasks_to_write, file)
